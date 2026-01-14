33e011c0bedd55be669d57be7cbab9b400b45610
Sean Don <seanyb05@gmail.com>
2026-01-13 20:46:41 -0500

    committing poetry configs

--- app/main.py
+++ app/main.py
@@ -1,33 +1,30 @@
 import logging
 import os
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 from datetime import datetime
 
 app = FastAPI()
 now = datetime.now()
 
-# Get the script's local directory
 base_dir = os.path.dirname(os.path.abspath(__file__))
 log_path = os.path.join(base_dir, f"{now}.log")
-
-# Configure logger to write to the local file
-logging.basicConfig(
-    filename=log_path,
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
-)
-logger = logging.getLogger(__name__)
 
 @app.post("/hello")
 def index():
     return {"message": "Hello World"}
 
-# @app.post("/log")
-# async def log_post_message(request: Request):
-#     # Receive the POST body
-#     body = await request.json()
-#
-#     # Log the received message locally
-#     logger.info(f"Received POST message: {body}")
-#
-#     return {"status": "Message logged locally", "data": body}
+@app.post("/log")
+async def log_post_message(request: Request):
+
+    # Receive the POST body
+    body = await request.json()
+
+    return {"status": "Message logged locally", "data": body}
+
+@app.get("/judgement")
+async def get_judgement():
+    return "you are bad"
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return Response(status_code=204) # 204 means "No Content"
